713df273288ce5873081f8effa1d6b3351b6da30
ape03 <ngcmpc@gmail.com>
2018-01-16 16:17:26 +0000

    @Test switchingTheCameraOffPowersDownTheSensor @Test pressingTheShutterWhenThePowerIsOffDoesNothing

--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -8,11 +8,30 @@ import static org.mockito.Mockito.*;
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor);
 
-        Sensor sen = mock(Sensor.class);
-        Camera cam = new Camera(sen);
+        camera.powerOn();
+        verify(sensor).powerUp();
+    }
 
-        cam.powerOn();
-        verify(sen).powerUp();
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor);
+
+        camera.powerOff();
+        verify(sensor).powerDown();
+    }
+
+    @Test
+    public void pressingTheShutterWhenThePowerIsOffDoesNothing() {
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor);
+
+        camera.powerOff();
+        verify(sensor).powerDown();
+        camera.pressShutter();
+        verifyNoMoreInteractions(sensor);
     }
 }
